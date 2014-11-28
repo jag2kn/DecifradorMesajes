798a28b839cfca9c181e1b28eb8217a27b705487
Jorge Gonzalez <jag2kn@gmail.com>
2014-11-28 13:00:21 -0500

    Se crea archivo para descargar base de datos de numeros primos de primes.utm.edu, se descargan y descomprimen en la carpeta Primos  * https://primes.utm.edu/lists/small/millions/

---
+++ descargarPrimos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in `seq 1 50`;
+do
+        wget -c https://primes.utm.edu/lists/small/millions/primes$i.zip -O Primos/primes$i.zip
+done
+
+find Primos -name '*.zip' -exec sh -c 'unzip -d Primos "$1"' _ {} \;
